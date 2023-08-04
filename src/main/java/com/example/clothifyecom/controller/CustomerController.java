1bdcf07ae87beca9e0c68150c2e601d0a9a5616e
Chanisa jayawardhana <sadin.chanisa8@gmail.com>
2023-08-04 16:13:05 +0530

    controller update

--- src/main/java/com/example/clothifyecom/controller/CustomerController.java
+++ src/main/java/com/example/clothifyecom/controller/CustomerController.java
@@ -1,4 +1,23 @@
 package com.example.clothifyecom.controller;
 
+import com.example.clothifyecom.entity.Customer;
+import com.example.clothifyecom.repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer){
+        return customerRepo.save(customer);
+    }
+
 }
