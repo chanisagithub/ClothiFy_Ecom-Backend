4cd75e176436c3d3b08d75168de0d18bff9f2c28
Chanisa jayawardhana <sadin.chanisa8@gmail.com>
2023-08-04 12:38:06 +0530

    dto

--- src/main/java/com/example/clothifyecom/dto/ItemRequestDTO.java
+++ src/main/java/com/example/clothifyecom/dto/ItemRequestDTO.java
@@ -1,4 +1,23 @@
 package com.example.clothifyecom.dto;
 
+import com.example.clothifyecom.enums.ItemCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ItemRequestDTO {
+    private String name;
+    private  double price;
+    private int Lqty;
+    private int Mqty;
+    private int Sqty;
+    private ItemCategory category;
+    private MultipartFile imgURL;
+
+
+
 }
