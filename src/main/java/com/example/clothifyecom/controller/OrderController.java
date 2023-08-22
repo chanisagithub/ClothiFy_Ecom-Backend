ee02d0bc59bea4238eea11a1f0beef58bc6973df
Chanisa jayawardhana <shashikajayawardena2001@gmail.com>
2023-08-22 21:54:28 +0530

    OrderCTRL

--- src/main/java/com/example/clothifyecom/controller/OrderController.java
+++ src/main/java/com/example/clothifyecom/controller/OrderController.java
@@ -1,5 +1,30 @@
 package com.example.clothifyecom.controller;
 
+
+import com.example.clothifyecom.repository.CustomerRepo;
+import com.example.clothifyecom.repository.ItemRepo;
+import com.example.clothifyecom.repository.OrderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
 
 }
